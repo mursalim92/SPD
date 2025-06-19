e89391d98298cdda2c346fd1ee33d319707e0dcc
mursalim92 <nevergiveup.jbaratama@gmail.com>
2025-06-19 09:44:13 +0800

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start(); 
-if (!isset($_SESSION['user_id'])) { 
-    header('Location: view/dahsboard.php'); 
-    exit(); // Selalu tambahkan exit() setelah header redirect 
-} 
-?> 
-
-

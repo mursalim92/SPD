7d71b2260dd7898ba77440535ff5bfcea6c1b1cb
mursalim92 <nevergiveup.jbaratama@gmail.com>
2025-06-19 09:36:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
-<?php
-session_start();
+<?php 
+session_start(); 
+if (!isset($_SESSION['user_id'])) { 
+    header('Location: view/dahsboard.php'); 
+    exit(); // Selalu tambahkan exit() setelah header redirect 
+} 
+?> 
 
-// Jika sudah login, arahkan ke dashboard
-if (isset($_SESSION['user_id'])) {
-    header('Location: view/dashboard.php');
-    exit;
-}
 
-// Jika belum login, arahkan ke form login
-header('Location: view/login.php');
-exit;
-?>
